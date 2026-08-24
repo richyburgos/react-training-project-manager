6a1d53b73873c2f89f5cfca329285ee30bf1cb65
Richard A. Burgos <richyburgos@live.com>
2026-08-23 21:52:00 -0600

    Added support for `textarea` input type in `LabeledInput` component and updated `ProjectForm` to use it for the description field.

--- src/components/common/LabeledInput.jsx
+++ src/components/common/LabeledInput.jsx
@@ -7,14 +7,23 @@ const LabeledInput= forwardRef(({inputType = "text", inputId, placeholder, label
         >
             {label}
         </label>
-        <input type={inputType}
-               ref={ref}
-               required
-               id={inputId}
-               name={inputId}
-               placeholder={placeholder}
-               className="px-3 py-2.5 text-sm text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 border border-slate-300"
-        />
+        { inputType !== "textarea" ?
+            <input type={inputType}
+                   ref={ref}
+                   required
+                   id={inputId}
+                   name={inputId}
+                   placeholder={placeholder}
+                   className="px-3 py-2.5 text-sm text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 border border-slate-300"
+            /> :
+            <textarea ref={ref}
+                      required
+                      id={inputId}
+                      name={inputId}
+                      placeholder={placeholder}
+                      className="px-3 py-2.5 text-sm text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 border border-slate-300"
+            />
+        }
     </div>;
 });
 
